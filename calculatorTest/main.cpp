e74b5d36a2677cb637c7e2d1887de6b17f9f565c
Melinda Fernandes <melindafernandes@melindas-macbook-pro.local>
2017-02-26 01:16:57 +0000

    Initial Commit

---
+++ calculatorTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  calculatorTest
+//
+//  Created by Melinda Fernandes on 26/02/2017.
+//  Copyright © 2017 Melinda Fernandes. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
